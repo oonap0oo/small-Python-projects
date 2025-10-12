e106c3a9db3ad0818f5fe9a0743cd1c3925194ae
oonap0oo <vnap0v@gmail.com>
2025-10-12 16:49:21 +0200

    Add files via upload

--- game_of_life7.py
+++ game_of_life7.py
@@ -43,7 +43,6 @@ import numpy as np
 
 # make new tkinter and canvas objects
 def generate_tk_canvas(height, width):
-    print("make tkinter and canvas objects")
     new_tk_root = tk.Tk()
     new_tk_root.title(txt)
     new_tk_root.resizable(False, False)
@@ -79,16 +78,16 @@ def drawfield(field, neighbours, cvs):
     cvs.delete('all')
     # draw vertical grid lines
     for x in x_grid:
-        cvs.create_line(x, 0, x, screen_height, fill = grid_color)
+        cvs.create_line(x, 0, x, y_grid[-1], fill = grid_color)
     # draw horizontal grid lines
     for y in y_grid:
-        cvs.create_line(0, y, screen_width, y, fill = grid_color)
+        cvs.create_line(0, y, x_grid[-1], y, fill = grid_color)
     # get row and column indexes of elements in array field equal to 1
     rows, cols = np.where(field == 1)
 
     cell_colors = colors[neighbours[rows, cols] % 2]
     # draw rectangles at locations in grid given by indexes in rows, cols
-    for y1, x1, y2, x2, cell_color in zip(rows * dx, cols * dy, rows * dx + dx, cols * dy + dy, cell_colors):
+    for x1, y1, x2, y2, cell_color in zip(cols * size, rows * size, cols * size + size, rows * size + size, cell_colors):
         cvs.create_rectangle(x1, y1, x2, y2, fill = cell_color, outline = grid_color)
     cvs.create_text(100, 40, text = f"{txt}\nGeneration: {gen_count}",
                     anchor = "nw", font = ("TkFixedFont",18,"normal"), fill = text_color)
@@ -102,14 +101,14 @@ def update_generation():
     drawfield(field1, neighbours1, canvas1)
     field1, neighbours1 = update_field_vectorized(field1)
     gen_count += 1
-    root1.after(dt, update_generation)
+    root1.after(dt, update_generation) # call this function again with time delay
 
-# stops program after mouse click
+# starts and stops program after mouse click
 def click_handler(event):
     global running # this function has to update the bool "running"
-    # event also has x & y attributes
-    if event.num == 1: # lef mouse button
+    if event.num == 1: # left mouse button
         if running:
+            root1.quit()
             root1.destroy() # if already running then stop
         else:
             running = True # if not running yet, start
@@ -119,14 +118,14 @@ def click_handler(event):
     
 # ------ parameters ------
 # number of cells in 2 dimensions
-Nrow = 80; Ncol = 120
-# image size in pixels
-screen_width = 1500; screen_height = 1000 
-# spacing between grid lines
-dx = int(screen_width / Ncol); dy = int(screen_height / Nrow)
+Nrow = 80; Ncol = 125
+# size of one square cell in pixels
+size = 13
+# image size in pixels, depends on number of rows, columns and cell size
+screen_width = size * Ncol; screen_height = size * Nrow
 # prepare x and y values at which to draw grid lines
-x_grid = np.arange(0, screen_width, dx)
-y_grid = np.arange(0, screen_height, dy)
+x_grid = np.array([n * size for n in range(Ncol+1)])
+y_grid = np.array([n * size for n in range(Nrow+1)])
 # time delay between steps in milliseconds
 dt = 30
 # title text
@@ -137,12 +136,13 @@ text_color = "#808080"
 #cell_color = "red"
 colors = np.array(("red","orange"))
 
-
 # initialise field
 # the field is a numpy array of integers
 # 0: dead/empty
 # 1: alive/occupied
 field1 = np.zeros((Nrow, Ncol), dtype = np.ubyte)
+# a second array which is to keep track of  number of neighbours for each field
+# only used to determine cell color at drawing of image
 neighbours1 = np.zeros((Nrow, Ncol), dtype = np.ubyte)
 
 # patterns
@@ -166,15 +166,18 @@ single_line_cols = np.array((1,2,2,3,4,5,6,7,8,10,11,12,13,14,18,19,20,27,28,29,
 # single block-laying switch engine
 block_layer_rows = np.array((0,0,0,0,1,2,2,3,3,3,4,4,4))
 block_layer_cols = np.array((0,1,2,4,0,3,4,1,2,4,0,2,4))
-
+# "Light-weight spaceship" moves sideways
+light_spaceship_rows = np.array((0,0,1,2,2,3,3,3,3))
+light_spaceship_cols = np.array((0,3,4,0,4,1,2,3,4))
 
 # insert patterns in field
 # adding the patterns to field1 by setting correct elements to 1
 # adding values to the row or column vectors defines the location in the field
 # subtracting a number with the vector flips the orientation of the pattern
-field1[gosper_gun_rows+10, gosper_gun_cols+5] = 1
-field1[gosper_gun_rows+10, 118-gosper_gun_cols] = 1
-
+field1[gosper_gun_rows+15, gosper_gun_cols+5] = 1
+field1[gosper_gun_rows+15, 118-gosper_gun_cols] = 1
+field1[light_spaceship_rows+5, light_spaceship_cols+45] = 1
+field1[light_spaceship_rows+70, 85-light_spaceship_cols] = 1
 
 # make tkinter root and canvas objects
 canvas1, root1 = generate_tk_canvas(screen_height, screen_width)
@@ -185,9 +188,11 @@ root1.bind("<Button>", click_handler)
 # global variable keeps track of number of generations calculated
 gen_count = 0
 
+# bool variable becomes true alfter user clicks in window
 running = False
 
-canvas1.create_text(screen_width // 2, screen_height // 2, text = "Click field to start",
+# text to warn user at start
+canvas1.create_text(screen_width // 2, screen_height // 2, text = "Click in window to start",
                     anchor = "center", font = ("TkFixedFont",30,"normal"), fill = "white")
 
 # tkinter main loop
